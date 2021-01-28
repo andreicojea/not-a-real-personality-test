1dc754f39687171a024de92dd5277bfc7df8c672
Andrei Cojea <andreicojea@gmail.com>
2021-01-28 20:28:52 +0200

    added main submodules & routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
+  },
+  {
+    path: 'quiz',
+    loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule)
+  },
+  {
+    path: 'result',
+    loadChildren: () => import('./result/result.module').then(m => m.ResultModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
