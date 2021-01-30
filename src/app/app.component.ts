3b19ec609d4f2224241341367e4e02b3c535e0a4
Andrei Cojea <andreicojea@gmail.com>
2021-01-30 21:47:40 +0200

    scroll top on page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'not-a-real-personality-test';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription!: Subscription;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.subscription = this.router.events.subscribe(evt => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
