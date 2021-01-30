1685441ec22a5b0e4a00ec643f8a7bf2799bb8ff
Andrei Cojea <andreicojea@gmail.com>
2021-01-30 12:55:58 +0200

    full height intro header via css, not js

--- src/app/landing/intro-header/intro-header.component.ts
+++ src/app/landing/intro-header/intro-header.component.ts
@@ -7,13 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroHeaderComponent implements OnInit {
 
-  static MIN_HEIGHT = 700;
-  fullHeight!: number;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.fullHeight = Math.max(window.innerHeight, IntroHeaderComponent.MIN_HEIGHT);
-  }
 
+  }
 }
