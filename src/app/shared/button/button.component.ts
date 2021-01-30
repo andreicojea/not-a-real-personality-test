3797fd0509ec2ed0094f82c0b42cb9c33c64823f
Andrei Cojea <andreicojea@gmail.com>
2021-01-30 12:50:34 +0200

    added button accent color

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
+
+  @Input() color = 'default';
+
+  @HostBinding('class') get colorClass(): string {
+    return `color-${this.color}`;
+  }
 
   constructor() { }
 
